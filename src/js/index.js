6644f370f6dde4e347396c01a49072ba50179aac
sairim <spiri@protonmail.com>
2019-09-02 21:49:32 +0300

    implement basic cache for searching

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,24 @@
-import { getResults } from './models/Search';
-import { getNowPlaying } from './models/NowPlaying';
-import * as resultsView from './views/resultsView';
-import { elements, renderSpinner, clearSpinner } from './views/base';
+import { getResults } from "./models/Search";
+import { getNowPlaying } from "./models/NowPlaying";
+import * as resultsView from "./views/resultsView";
+import { elements, renderSpinner, clearSpinner } from "./views/base";
 
 /** Global state of the App
  * - Search
  * - Current movie object
  */
 const state = {
-   showingNowPlaying: true,
-   nowPlaying: []
+  showingNowPlaying: true,
+  resultsCache: {}
 };
 
 /**
  * Set header
  */
 const setHeader = () => {
-  const header = document.querySelector('h1');
-  header.innerText = state.showingNowPlaying ? 'Now Playing' : 'Search Results';
-}
+  const header = document.querySelector("h1");
+  header.innerText = state.showingNowPlaying ? "Now Playing" : "Search Results";
+};
 
 /**
  * Now Playing Controller
@@ -35,49 +35,55 @@ const controlNowPlaying = async () => {
   try {
     const results = await getNowPlaying(1);
     clearSpinner();
-    state.nowPlaying = results;
+    state.resultsCache["nowPlaying"] = results;
     resultsView.renderResults(results);
-  } catch(err) {
+  } catch (err) {
     console.log(err);
     clearSpinner();
   }
-}
+};
 
 (function initState() {
   setHeader();
   controlNowPlaying();
 })();
 
- /**
-  * Search controller
-  */
+/**
+ * Search controller
+ */
 const controlSearch = async () => {
   const query = resultsView.getInput();
 
   if (query) {
     state.showingNowPlaying = false;
     setHeader();
-    
+
     resultsView.clearResults();
     renderSpinner(elements.searchRes);
-    try {
-      const results = await getResults(query, 1);
+
+    if (state.resultsCache[query]) {
       clearSpinner();
-      resultsView.renderResults(results)
-    } catch (err) {
-      console.log(err);
-      clearSpinner();
+      resultsView.renderResults(state.resultsCache[query]);
+    } else {
+      try {
+        const results = await getResults(query, 1);
+        state.resultsCache[query] = results;
+        clearSpinner();
+        resultsView.renderResults(results);
+      } catch (err) {
+        console.log(err);
+        clearSpinner();
+      }
     }
   } else {
     state.showingNowPlaying = true;
     setHeader();
     resultsView.clearResults();
-    resultsView.renderResults(state.nowPlaying);
+    resultsView.renderResults(state.resultsCache.nowPlaying);
   }
-}
+};
 
-elements.searchInput.addEventListener('input', e => {
+elements.searchInput.addEventListener("input", e => {
   e.preventDefault();
   controlSearch();
-})
-
+});
